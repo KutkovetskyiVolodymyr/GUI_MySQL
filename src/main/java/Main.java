bbdd4b93dcc989625550e49c20d1b354329e6faa
Software18Engineering <kutkoveckij@gmail.com>
2017-04-26 22:31:50 +0300

    Create 1 window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,5 @@
  * Created by kutkovetskiy on 26.04.2017.
  */
 public class Main {
+    Window1 w1 = new Window1();
 }
--- src/main/java/Window1.java
+++ src/main/java/Window1.java
@@ -1,6 +1,26 @@
+import javax.swing.*;
+
 /**
  * Created by kutkovetskiy on 26.04.2017.
  */
 public class Window1 {
+    JFrame frame;
+    JButton button1,button2;
+    JPanel jPanel= new JPanel();
+    JPanel jPanelBig=new JPanel();
+
+    Window1() {
+        frame = new JFrame();
+        button1 = new JButton("Відкрити");
+        button2 = new JButton("Запрос");
+    }
+    void frameG () {
+        frame.getContentPane().add(jPanelBig);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450, 700);
+        frame.setVisible(true);
+        frame.repaint();
+        frame.setLocationRelativeTo(null);
+    }
 
 }
