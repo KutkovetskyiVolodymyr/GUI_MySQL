be64128b5c03081918110596f7592adaa8269f98
Software18Engineering <kutkoveckij@gmail.com>
2017-04-27 19:16:23 +0300

    Create SQL.java, addition of another.

---
+++ src/main/java/SQL.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.sql.*;
+
+/**
+ * Created by kutkovetskiy on 27.04.2017.
+ */
+public class SQL {
+    public Connection con =null;
+    Statement stmt;
+    ResultSet rs;
+
+    String namebd1,url,user,password;
+
+    SQL(String ip,String port1,String user1, String password1,String namebd){
+        url="jdbc:mysql://"+ip+":"+port1+"/"+namebd;
+        namebd1=namebd;
+        user=user1;
+        password=password1;
+    }
+    public void mySQLConect(){
+        try {
+            con = DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException sqlEx) {
+            JOptionPane.showMessageDialog(null, "Wrong");
+        }
+    }
+    public void closedb() throws SQLException {
+        con.close();
+        stmt.close();
+        rs.close();
+    }
+
+}
+
--- src/main/java/Window1.java
+++ src/main/java/Window1.java
@@ -12,13 +12,35 @@ public class Window1 implements ActionListener {
     JPanel jPanel= new JPanel();
     JPanel jPanelBig=new JPanel();
     JPanel jPanelmin=new JPanel();
+    JTextField tf1, tf2,tf4,tf5;
+    JPasswordField tf3;
 
     Window1() {
         frame = new JFrame();
         button1 = new JButton("Відкрити");
-
         button1.addActionListener(this);
-        button1.setPreferredSize(new Dimension(100,20));
+        button1.setSize(100,50);
+        button1.setLocation(50,250);
+
+        tf1 = new JTextField();
+        tf1.setSize(170,25);
+        tf1.setLocation(250,1);
+
+        tf2 = new JTextField();
+        tf2.setSize(170,25);
+        tf2.setLocation(250,50);
+
+        tf3 = new JPasswordField();
+        tf3.setSize(170,25);
+        tf3.setLocation(250,100);
+
+        tf4 = new JTextField();
+        tf4.setSize(170,25);
+        tf4.setLocation(250,150);
+
+        tf5 = new JTextField();
+        tf5.setSize(170,25);
+        tf5.setLocation(250,200);
 
         Icon leftIcon = UIManager.getIcon("OptionPane.errorIcon");
         leftButton = new JButton("Вихід");
@@ -44,6 +66,11 @@ public class Window1 implements ActionListener {
         jPanelmin.add(new JLabel(new ImageIcon(getClass().getClassLoader().getResource("database (1).jpg"))));
         jPanel.add(leftButton);
         jPanel.add(button1);
+        jPanel.add(tf1);
+        jPanel.add(tf2);
+        jPanel.add(tf3);
+        jPanel.add(tf4);
+        jPanel.add(tf5);
 
         frame.setVisible(true);
         frame.repaint();
