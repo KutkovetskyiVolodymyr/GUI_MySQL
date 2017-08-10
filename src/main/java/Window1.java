9b7798ebecb2d6732f527f2a0af01f79e838e972
Vladimir Kutkovetskiy <kutkoveckij@gmail.com>
2017-08-10 18:22:23 +0300

    Update Window1.java

--- src/main/java/Window1.java
+++ src/main/java/Window1.java
@@ -5,48 +5,36 @@ import java.awt.event.ActionListener;
 
 /**
  * Created by kutkovetskiy on 26.04.2017.
+ * Вікно авторизації
  */
 public class Window1 implements ActionListener {
+   
     JFrame frame;
     JButton button1,leftButton;
-    JPanel jPanel= new JPanel();
-    JPanel jPanelBig=new JPanel();
+    JPanel jPanel;
+    JPanel jPanelBig;
     JPanel jPanelmin=new JPanel();
     JTextField tf1, tf2,tf4,tf5;
     JPasswordField tf3;
-
+    Icon leftIcon
+    
     Window1() {
+        
         frame = new JFrame();
+        jPanel= new JPanel();
+        jPanelBig=new JPanel();
         button1 = new JButton("Відкрити");
         button1.addActionListener(this);
-        button1.setSize(100,50);
-        button1.setLocation(50,250);
 
         tf1 = new JTextField();
-        tf1.setSize(170,25);
-        tf1.setLocation(250,1);
-
         tf2 = new JTextField();
-        tf2.setSize(170,25);
-        tf2.setLocation(250,50);
-
         tf3 = new JPasswordField();
-        tf3.setSize(170,25);
-        tf3.setLocation(250,100);
-
         tf4 = new JTextField();
-        tf4.setSize(170,25);
-        tf4.setLocation(250,150);
-
         tf5 = new JTextField();
-        tf5.setSize(170,25);
-        tf5.setLocation(250,200);
 
-        Icon leftIcon = UIManager.getIcon("OptionPane.errorIcon");
+        leftIcon = UIManager.getIcon("OptionPane.errorIcon");
         leftButton = new JButton("Вихід");
         leftButton.setVisible(true);
-        leftButton.setSize(150,50);
-        leftButton.setLocation(200,250);
         leftButton.setIcon(leftIcon);
         leftButton.addActionListener(this);
 
@@ -61,7 +49,6 @@ public class Window1 implements ActionListener {
         frame.getContentPane().add(jPanelBig);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(450, 700);
-
 
         jPanelmin.add(new JLabel(new ImageIcon(getClass().getClassLoader().getResource("database (1).jpg"))));
         jPanel.add(leftButton);
